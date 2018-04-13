9895716709c48ec5e87ebaec866e8fc761cf7879
Chris Foytek <cff15@txstate.edu>
2018-04-13 12:19:23 -0500

    Fixed file structure

--- mypwd/src/mypwd.c
+++ mypwd/src/mypwd.c
@@ -1,6 +1,22 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
-    
+void mypwd()
+{
+  char path[PATH_MAX];
+  if(getcwd(path, sizeof(path)) == NULL)
+    perror("getcwd() error");
+  else
+    printf("%s\n", path);
+}
+
+
+int main()
+{
+  mypwd();  
+
+  return 0;        
 }
