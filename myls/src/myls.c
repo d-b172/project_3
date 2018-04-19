f95ef44f1b80615595e5b542679d3d0373860df1
MiguelJN <mjn41@txstate.edu>
2018-04-19 14:56:03 -0500

    asdfasdflk

--- myls/src/myls.c
+++ myls/src/myls.c
@@ -13,7 +13,9 @@
 
 int L_FLAG = 0;
 int ARG_FLAG = 0;
+int DIR_FLAG =0;
 char *in_args[MAX_SIZE];
+char *dirs[MAX_SIZE];
 struct stat buf;
 char permissions[11];
 char timebuf[50];
@@ -44,9 +46,61 @@ int main(int argc, char **argv) {
             j++;
         }
         in_args[j] = NULL;
+        qsort (in_args, j, sizeof(char*), comp);
         /* If we pass myls a directory as an argument, we can open the directory
            and get all the files in it and print ls infor about them, do this for all
-           directories passed */  
+           directories passed */
+        int dir_count = 0;
+        for(i = 0; i < j; i++){
+            stat(in_args[i],&buf);
+            if(S_ISDIR(buf.st_mode)){
+                DIR_FLAG = 1;
+                dirs[dir_count] = malloc((strlen(in_args[i]) + 1) *sizeof(char));
+                strncpy(dirs[dir_count], in_args[i], strlen(in_args[i]) + 1);
+                dir_count++;
+            }
+            else {
+                if(L_FLAG){
+                    stat(in_args[i],&buf);
+                    permissions[0] = (S_ISDIR(buf.st_mode) ? 'd' : '-');
+                    permissions[1] = (buf.st_mode & S_IRUSR) ? 'r' : '-';
+                    permissions[2] = (buf.st_mode & S_IWUSR) ? 'w' : '-';
+                    permissions[3] = (buf.st_mode & S_IXUSR) ? 'x' : '-';
+                    permissions[4] = (buf.st_mode & S_IRGRP) ? 'r' : '-';
+                    permissions[5] = (buf.st_mode & S_IWUSR) ? 'w' : '-';
+                    permissions[6] = (buf.st_mode & S_IXUSR) ? 'x' : '-';
+                    permissions[7] = (buf.st_mode & S_IROTH) ? 'r' : '-';
+                    permissions[8] = (buf.st_mode & S_IWOTH) ? 'w' : '-';
+                    permissions[9] = (buf.st_mode & S_IXOTH) ? 'x' : '-';
+                    permissions[10] = '\0';
+                    numLinks = buf.st_nlink;
+                    strncpy(timebuf, ctime(&buf.st_atime), 49);
+                    timebuf[49] = '\0';
+                    int len = strlen(timebuf);
+                    if(timebuf[len-1]=='\n') timebuf[len-1] = '\0';
+                    int size = buf.st_size;
+                    struct passwd *user = getpwuid(buf.st_uid);
+                    struct group *grp = getgrgid(buf.st_gid);
+                    char u[50];
+                    strncpy(u, user->pw_name, 49);
+                    u[49] = '\0';
+                    char g[50];
+                    strncpy(g, grp->gr_name, 49);
+                    g[49] = '\0';
+                    
+                    printf("%s %d %s %s %d %s %s\n", permissions, numLinks, u, g, size, timebuf, in_args[i]);
+                }
+                else
+                    printf("%s ",in_args[i]);
+            }
+        }
+        printf("\n");
+        if(DIR_FLAG){
+            for(i = 0; i < dir_count; i++){
+                printf("\n%s: \n", dirs[i]);
+                stat_ls_dir(dirs[i]);
+            }
+        }
     }
     else if(argsind == argc) { //If no args
         //Do command for all files in directory
@@ -64,6 +118,7 @@ int main(int argc, char **argv) {
         }
         int i;
         in_args[j] = NULL;
+        qsort (in_args, j, sizeof(char*), comp);
         if(L_FLAG){
             for(i = 0; i < j; i++){
                 stat(in_args[i],&buf);
@@ -92,12 +147,11 @@ int main(int argc, char **argv) {
                 char g[50];
                 strncpy(g, grp->gr_name, 49);
                 g[49] = '\0';
-                qsort (in_args, j, sizeof(char*), comp);
+                
                 printf("%s %d %s %s %d %s %s\n", permissions, numLinks, u, g, size, timebuf, in_args[i]);
             }
         }
         else{
-            qsort (in_args, j, sizeof(char*), comp);
             for(i = 0; i < j; i++) {
                 printf("%s ", in_args[i]);
             }
@@ -107,11 +161,65 @@ int main(int argc, char **argv) {
     
 }
 
-
+void stat_ls_dir(char* dirs){
+    DIR *dir = opendir(dirs);
+    struct dirent *dp;
+        char *file_name;
+        int j = 0;
+        while((dp = readdir(dir)) != NULL) {
+            file_name = dp->d_name;
+            if(strcmp(file_name, ".") != 0 && strcmp(file_name, "..") != 0) { //Demorgan's law strikes again
+                in_args[j] = malloc((strlen(file_name) + 1) * sizeof(char));
+                strncpy(in_args[j], file_name, strlen(file_name) + 1);
+                j++;
+            } 
+        }
+        int i;
+        in_args[j] = NULL;
+        qsort (in_args, j, sizeof(char*), comp);
+        if(L_FLAG){
+            for(i = 0; i < j; i++){
+                stat(in_args[i],&buf);
+                permissions[0] = (S_ISDIR(buf.st_mode) ? 'd' : '-');
+                permissions[1] = (buf.st_mode & S_IRUSR) ? 'r' : '-';
+                permissions[2] = (buf.st_mode & S_IWUSR) ? 'w' : '-';
+                permissions[3] = (buf.st_mode & S_IXUSR) ? 'x' : '-';
+                permissions[4] = (buf.st_mode & S_IRGRP) ? 'r' : '-';
+                permissions[5] = (buf.st_mode & S_IWUSR) ? 'w' : '-';
+                permissions[6] = (buf.st_mode & S_IXUSR) ? 'x' : '-';
+                permissions[7] = (buf.st_mode & S_IROTH) ? 'r' : '-';
+                permissions[8] = (buf.st_mode & S_IWOTH) ? 'w' : '-';
+                permissions[9] = (buf.st_mode & S_IXOTH) ? 'x' : '-';
+                permissions[10] = '\0';
+                numLinks = buf.st_nlink;
+                strncpy(timebuf, ctime(&buf.st_atime), 49);
+                timebuf[49] = '\0';
+                int len = strlen(timebuf);
+                if(timebuf[len-1]=='\n') timebuf[len-1] = '\0';
+                int size = buf.st_size;
+                struct passwd *user = getpwuid(buf.st_uid);
+                struct group *grp = getgrgid(buf.st_gid);
+                char u[50];
+                strncpy(u, user->pw_name, 49);
+                u[49] = '\0';
+                char g[50];
+                strncpy(g, grp->gr_name, 49);
+                g[49] = '\0';
+                
+                printf("%s %d %s %s %d %s %s\n", permissions, numLinks, u, g, size, timebuf, in_args[i]);
+            }
+        }
+        else{
+            for(i = 0; i < j; i++) {
+                printf("%s ", in_args[i]);
+            }
+            printf("\n");
+        }
+}
 
 int comp (const void * elem1, const void * elem2) 
 {
-    char f = *((char*)elem1);
+    char f = *((char*)elem1); 
     char s = *((char*)elem2);
     if (f < s) return  1;
     if (f > s) return -1;
